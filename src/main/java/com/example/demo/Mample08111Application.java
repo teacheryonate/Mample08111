8babc5f175330991434fb6e8beffc2a936ef26c7
tj705 <a@a.com>
2023-08-11 14:30:50 +0900

    first work

---
+++ src/main/java/com/example/demo/Mample08111Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mample08111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mample08111Application.class, args);
+	}
+
+}
